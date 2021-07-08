022d74f0c8aece43462ce7a754211769dd01e972
isha <isha@isha.isha>
2021-07-08 09:24:59 +0200

    Create Custom Components

---
+++ src/app/custom_components/custom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'custom-components',
+    templateUrl: './custom.component.html',
+    styleUrls: ['./custom.component.scss']
+})
+
+export class CustomComponent {
+   
+}
